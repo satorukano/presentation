1e980f9f9acb1ca4a01cc9a2428d0c96d2c3cfbd
satorukano <kiritomo0206@icloud.com>
2024-07-01 22:13:47 +0900

    fix

--- presentation.java
+++ presentation.java
@@ -25,3 +25,19 @@ public void testAdd() {
     assertEquals(-1, calculator.add(-2, 1));
     assertEquals(-5, calculator.add(-2, -3));
 }
+
+
+
+
+
+
+public boolean isEligible(int age, double salary, boolean isEmployed, boolean hasDegree) {
+    if ((age > 20 && age < 30 && salary > 40000.0 && isEmployed) ||
+        (age > 25 && hasDegree) ||
+        (!isEmployed && salary < 30000.0 && hasDegree)) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
