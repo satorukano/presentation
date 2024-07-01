6d1422b6fe881f52a4c24ea8034f258b81b2aa10
satorukano <kiritomo0206@icloud.com>
2024-07-01 16:45:06 +0900

    first commit

---
+++ presentation.java
@@ -0,0 +1,27 @@
+import java.beans.Transient;
+
+@Test
+public void testAdd() {
+    Calculator Calculator = new Calcurator();
+    assertEquals(5, Calculator.add(2, 3));
+}
+
+class Calculator {
+    public int add(int a, int b) {
+        return a + b;
+    }
+}
+
+class Calculator {
+    public int add(int a, int b) {
+        return a - b;
+    }  
+}
+
+@Test
+void testAdd() {
+    Calculator calculator = new Calculator();
+    assertEquals(5, calculator.add(2, 3));
+    assertEquals(-1, calculator.add(-2, 1));
+    assertEquals(-5, calculator.add(-2, -3));
+}
